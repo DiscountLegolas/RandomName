66a55f22793b63d4c37fca89c4c5d7fc5984bdff
DiscountLegolas <69970115+DiscountLegolas@users.noreply.github.com>
2022-01-12 16:00:29 +0300

    Proje dosyası ekle.

---
+++ RandomName/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomName.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomName.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Names.TurkishName = null;
+            Names.MaleName = null;
+            Names.FemaleName = null;
+            return View();
+        }
+        public IActionResult YeniİsimOluştur()
+        {
+            var vs = System.IO.File.ReadAllLines(@"C:\Users\prof1\source\repos\RandomName\RandomName\isimler.txt");
+            Names.TurkishName = vs[new Random().Next(0, vs.Length - 1)];
+            return View("~/Views/Home/Index.cshtml");
+        }
+        public IActionResult GenerateRandomEnglishMaleName()
+        {
+            var vs = System.IO.File.ReadAllLines(@"C:\Users\prof1\source\repos\RandomName\RandomName\Male.txt");
+            Names.MaleName = vs[new Random().Next(0, vs.Length - 1)];
+            return View("~/Views/Home/Index.cshtml");
+        }
+        public IActionResult GenerateRandomEnglishFemaleName()
+        {
+            var vs = System.IO.File.ReadAllLines(@"C:\Users\prof1\source\repos\RandomName\RandomName\Female.txt");
+            Names.FemaleName = vs[new Random().Next(0, vs.Length - 1)];
+            return View("~/Views/Home/Index.cshtml");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RandomName/Models/Names.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomName.Models
+{
+    public class Names
+    {
+        public static string TurkishName { get; set; }
+        public static string MaleName { get; set; }
+        public static string FemaleName { get; set; }
+
+    }
+}
